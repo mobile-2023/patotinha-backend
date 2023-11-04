29da6c2d0147a1082dc22641c79b030684606ca7
florskdev <florskdev@gmail.com>
2023-11-04 13:49:06 -0300

    changing back for express

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,64 @@
+import { PrismaClient } from '@prisma/client';
 import express, { Express, Request, Response } from "express";
 import 'dotenv/config'
 
 const app: Express = express();
+const prisma = new PrismaClient()
 const port = process.env.PORT;
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
-	res.send('<h1>Hello World!</h1>');
+    res.send('<h1>Hello World!</h1>');
 })
 
-app.listen(port, () => {
-	console.log(`Servidor iniciado na porta: ${port}`);
+app.get('/users', async (request, reply) => {
+    const users = await prisma.user.findMany()
+    return users
+})
+
+app.post('/users', async (request, reply) => {
+
+    console.log(request.body)
+
+    const { username, email, password } = request.body
+
+    const isRegistered = await prisma.user.findMany({ where: { email: email } })
+
+    if (isRegistered.length === 0) {
+        await prisma.user.create({
+            data: {
+                username,
+                email,
+                password
+            }
+        })
+
+        reply.sendStatus(200)
+    } else {
+        reply.sendStatus(401)
+    }
+})
+
+/* Authentication */
+app.post('/users/login', async (request, reply) => {
+
+    const { email, password } = request.body
+
+    const isRegistered = await prisma.user.findMany({ where: { email: email } })
+
+    if (isRegistered.length > 0) {
+        if (password === isRegistered[0].password) {
+            console.log("Logando")
+        } else {
+            console.log("Senha incorreta")
+        }
+    } else {
+        reply.status(401)
+    }
+
+})
+
+app.listen(3333, () => {
+    console.log(`Servidor iniciado na porta: 3333`);
 })
