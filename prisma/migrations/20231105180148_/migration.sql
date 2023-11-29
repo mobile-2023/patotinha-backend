eff339c9d4451d3152e27c0993627e51c7a49e4f
SamuelLemosS <samuellemosesilva@gmail.com>
2023-11-29 20:08:31 -0300

    chore: spring start

--- prisma/migrations/20231105180148_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `rating` on the `Comment` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Comment" DROP COLUMN "rating";
---
+++ src/main/java/demo/restservices/entities/GameList.java
@@ -0,0 +1,5 @@
+package demo.restservices.controller;
+
+public class GameList {
+    
+}
