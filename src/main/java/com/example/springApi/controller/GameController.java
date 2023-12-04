d8a2fc2b33e97fb7737cde4070528a2315d29c04
Suelen <105010254+SuelenRamos@users.noreply.github.com>
2023-12-04 21:01:38 +0000

    Controllers game and gamelist

--- src/main/java/com/example/springApi/controller/GameController.java
+++ src/main/java/com/example/springApi/controller/GameController.java
@@ -1,11 +1,61 @@
 package com.example.springApi.controller;
 
-import com.example.springApi.repository.IGameListRepository;
+import com.example.springApi.model.Game;
+import com.example.springApi.repository.IGameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/game")
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/game")
 public class GameController {
+
     @Autowired
-    private IGameListRepository gameListRepository;
+    private IGameRepository gameRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllGames() {
+        List<Game> games = gameRepository.findAll();
+        return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGameById(@PathVariable UUID id) {
+        Optional<Game> optionalGame = gameRepository.findById(id);
+        return optionalGame.map(game -> new ResponseEntity<>(game, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> createGame(@RequestBody Game game) {
+        Game createdGame = gameRepository.save(game);
+        return new ResponseEntity<>(createdGame, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable UUID id, @RequestBody Game game) {
+        Optional<Game> optionalGame = gameRepository.findById(id);
+        if (optionalGame.isPresent()) {
+            game.setId(id);
+            Game updatedGame = gameRepository.save(game);
+            return new ResponseEntity<>(updatedGame, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable UUID id) {
+        if (gameRepository.existsById(id)) {
+            gameRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/example/springApi/controller/GameListController.java
+++ src/main/java/com/example/springApi/controller/GameListController.java
@@ -1,11 +1,61 @@
 package com.example.springApi.controller;
 
+import com.example.springApi.model.GameList;
 import com.example.springApi.repository.IGameListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/gameList")
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/gamelist")
 public class GameListController {
+
     @Autowired
     private IGameListRepository gameListRepository;
+
+    @GetMapping
+    public ResponseEntity<List<GameList>> getAllGameLists() {
+        List<GameList> gameLists = gameListRepository.findAll();
+        return new ResponseEntity<>(gameLists, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameList> getGameListById(@PathVariable UUID id) {
+        Optional<GameList> optionalGameList = gameListRepository.findById(id);
+        return optionalGameList.map(gameList -> new ResponseEntity<>(gameList, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<GameList> createGameList(@RequestBody GameList gameList) {
+        GameList createdGameList = gameListRepository.save(gameList);
+        return new ResponseEntity<>(createdGameList, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GameList> updateGameList(@PathVariable UUID id, @RequestBody GameList gameList) {
+        Optional<GameList> optionalGameList = gameListRepository.findById(id);
+        if (optionalGameList.isPresent()) {
+            gameList.setId(id);
+            GameList updatedGameList = gameListRepository.save(gameList);
+            return new ResponseEntity<>(updatedGameList, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGameList(@PathVariable UUID id) {
+        if (gameListRepository.existsById(id)) {
+            gameListRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
